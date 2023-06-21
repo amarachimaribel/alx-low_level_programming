21a151237b520de1ab2866e4c9bf7b483166f880
amarachimaribel <amarachimaribel266@gmail.com>
2023-06-21 13:34:03 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,7 +6,7 @@
  * Return: 1 for alphabetic character or 0 for anything else
  */
 int _isalpha(int c)
-if ((c >= 65 && c <= 97) || (c >= 97 && c <= 122))
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 {
 return (1);
 return (0);

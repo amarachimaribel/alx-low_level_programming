44c6c9b4a3268011ebd7cb6eb589bef71568a083
amarachimaribel <amarachimaribel266@gmail.com>
2023-06-20 18:44:03 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
-/**
-* main - print _putchar
-* is written by mbah
-* Return: 0
-*/
 
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 int main(void)
 {
 _putchar('_');
@@ -18,4 +18,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
- 
+
